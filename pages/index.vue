206303376e5f1fc81b5b1a78b4bce7e15dfae1dc
wuwu <wwwuwu@gmail.com>
2019-07-15 19:46:10 +0200

    fix about & kontakt sektion

--- pages/index.vue
+++ pages/index.vue
@@ -31,12 +31,7 @@ export default {
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
+<style lang='scss'>
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -44,5 +39,50 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+#yella-venohr .body {
+  img {
+    width: 250px;
+    height: auto;
+  }
+  p:nth-child(3) {
+    text-align: center;
+  }
+  @include tablet {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 30px;
+    p:first-child {
+      grid-column: 2 / 4;
+      grid-row: 1;
+    }
+    p:nth-child(2) {
+      grid-column: 2 / 4;
+      grid-row: 2;
+    }
+    p:nth-child(3) {
+      grid-column: 1 / 2;
+      grid-row: 1 / 3;
+      text-align: left;
+    }
+  }
+}
+
+#kontakt .body {
+  @include tablet {
+    p:nth-child(4){
+      float: left;
+      line-height: 36px;
+      width:40%;
+    }
+    img{
+      padding: 15px 0 0;
+    }
+    p:nth-child(5){
+      float: left;
+      line-height: 36px;
+    }
+  }
 }
 </style>
